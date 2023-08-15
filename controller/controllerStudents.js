7af2589100f7343d19560d57ae4b2c36531b095e
Sergio <93742269+Remisbagg@users.noreply.github.com>
2023-08-14 22:21:05 -0500

    Update controllerStudents.js

--- controller/controllerStudents.js
+++ controller/controllerStudents.js
@@ -4,20 +4,20 @@ import { insertTitleList } from "../view/viewStudents.js";
 
 const buttonCall = document.getElementById("btnListar");
 
-//Obtenido del Modelo
-const getListStudents = (students) =>{
-    let datos = [];
-    for (let s of students){
-        let datosTemplate = `${s.name} ${s.lastName} ${s.age}`;
-        datos.push(datosTemplate);
-    }
-    return datos;
+// Obtenido del Modelo
+const getListStudents = (students) => {
+  let datos = [];
+  for (let s of students) {
+    let datosTemplate = `${s.name} ${s.lastName} ${s.age}`;
+    datos.push(datosTemplate);
+  }
+  return datos;
 }
-//Asociando el evento al boton
 
-buttonCall.addEventListener("click", () =>{
-    const response = getListStudents(students);
-    //console.log(response);
-    studentsViewList(response);
-    insertTitleList();
+// Asociando el evento al boton
+buttonCall.addEventListener("click", () => {
+  const response = getListStudents(students);
+  //console.log(response);
+  studentsViewList(response);
+  insertTitleList();
 });
